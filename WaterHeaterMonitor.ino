3af11b2bb5551807fc7751d5405a4c5c957040bc
Sigbjørn Trageton <sigbjorn@trageton.net>
2023-01-17 17:23:36 +0100

    Rewrite

--- WaterHeaterMonitor.ino
+++ WaterHeaterMonitor.ino
@@ -1,44 +1,59 @@
-#include <HomeSeerClient.h>
+#include <HomeSeerClientESP8266.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-HomeSeerClient homeSeerClient;
+#define temperatureBottomPin D4
+OneWire oneWire(temperatureBottomPin); 
 
-const short temperatureBottomPin = A0;
-const short temperatureTopPin = A1;
+DallasTemperature sensors(&oneWire);
+HomeSeerClientESP8266 homeSeerClient;
 
-const int heaterBottomTempProbeId = 2444;
-const int heaterTopTempProbeId = 2445;
-const int heaterMonitorStatusId = 2446;
+const int heaterBottomTempProbeId = 2310;
+const int heaterTopTempProbeId = 2280;
+const int heaterMonitorStatusId = 2281;
 
-unsigned long previousAliveMillis;
+unsigned long previousAliveMillis = 0;
 const long aliveInterval = 3*60*1000L;
 
-unsigned long previousSensorReadingsMillis;
-const long sensorReadingsInterval = 3*60*1000L;
+unsigned long previousSensorReadingsMillis = 0;
+const long sensorReadingsInterval = 60*1000L;
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial) continue;
-
+    Serial.println("Setting up! Please wait...");
+    sensors.begin();
     homeSeerClient.init();
 }
 
 void loop()
 {
+    checkTemperatureSensors();
     sendAlive();
 }
 
 void checkTemperatureSensors()
 {
-    float bottomSensorReading;
     float topSensorReading;
     if(millis() - previousSensorReadingsMillis >= sensorReadingsInterval)
     {
+        Serial.println("Reading sensor");
         previousSensorReadingsMillis += sensorReadingsInterval;
-        bottomSensorReading = analogRead(heaterBottomTempProbeId);
-        topSensorReading = analogRead(heaterTopTempProbeId);
-        homeSeerClient.updateHomeSeer(heaterBottomTempProbeId, bottomSensorReading);
-        homeSeerClient.updateHomeSeer(heaterTopTempProbeId, topSensorReading);
+        sensors.requestTemperatures();
+        float bottomSensorReading = sensors.getTempCByIndex(0);
+        if(bottomSensorReading != DEVICE_DISCONNECTED_C) 
+        {
+            Serial.print("Temp bottom: ");
+            Serial.println(bottomSensorReading);
+            // topSensorReading = analogRead(heaterTopTempProbeId);
+            homeSeerClient.updateHomeSeer(heaterBottomTempProbeId, bottomSensorReading);
+            // homeSeerClient.updateHomeSeer(heaterTopTempProbeId, topSensorReading);
+        }
+        else
+        {
+            Serial.println("Error: Could not read temperature data");
+        }
     }
 }
 
@@ -46,6 +61,7 @@ void sendAlive() {
     if(millis() - previousAliveMillis >= aliveInterval)
     {
         previousAliveMillis += aliveInterval;
+        Serial.println("Alive");
         homeSeerClient.updateHomeSeer(heaterMonitorStatusId, 100);
     }
 }
